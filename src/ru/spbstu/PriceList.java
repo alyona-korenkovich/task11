70ede0c204bfd3a4fc04dab2760f4eea531acba8
Alyona Korenkovich <elena.lubickaya@mail.ru>
2020-02-17 14:32:21 +0300

    added "whatIsThePrice" method

--- src/ru/spbstu/PriceList.java
+++ src/ru/spbstu/PriceList.java
@@ -24,6 +24,8 @@ public class PriceList {
         }
         if (index != -1) {
             priceList.remove(index);
+        } else {
+            System.out.println("Item with this name doesn't exist. Try again.");
         }
     }
 
@@ -37,7 +39,41 @@ public class PriceList {
         }
         if (index != -1) {
             priceList.get(index).changePrice(newPrice);
+        } else {
+            System.out.println("Item with this name doesn't exist. Try again.");
         }
+    }
+
+    public void changeItemName(String name, String newName){
+        int index = -1;
+        for (Item element : priceList) {
+            if (element.getName().equals(name)) {
+                index = priceList.indexOf(element);
+                break;
+            }
+        }
+        if (index != -1) {
+            priceList.get(index).changeName(newName);
+        } else {
+            System.out.println("Item with this name doesn't exist. Try again.");
+        }
+    }
+
+    public double whatIsThePrice(int code, int amount){
+        int index = -1;
+        double currentPrice = 0;
+        for (Item element : priceList) {
+            if (element.getCode() == code) {
+                index = priceList.indexOf(element);
+                break;
+            }
+        }
+        if (index != 1) {
+            currentPrice = amount * priceList.get(index).getPrice();
+        } else {
+            System.out.println("Item with this code doesn't exist. Try again.");
+        }
+        return currentPrice;
     }
 
     public boolean equals(Object obj) {
--- test/ru/spbstu/PriceListTest.java
+++ test/ru/spbstu/PriceListTest.java
@@ -10,6 +10,7 @@ public class PriceListTest {
     Item item1 = new Item ("mouse", 11984, 599.99);
     Item item2 = new Item ("screen", 19853, 5899.99);
     Item item1Changed = new Item ("mouse", 11984, 999.99);
+    Item item1Changed2 = new Item ("mouse 3000", 11984, 599.99);
 
     @Test
     public void setItem() {
@@ -58,5 +59,39 @@ public class PriceListTest {
         PriceList expected = new PriceList(testList);
 
         Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void changeItemName() {
+        PriceList actual = new PriceList(new ArrayList<Item>());
+        actual.setItem(item);
+        actual.setItem(item1);
+        actual.setItem(item2);
+        actual.changeItemName(item1.name, "mouse 3000");
+
+        ArrayList<Item> testList = new ArrayList<>();
+        testList.add(item);
+        testList.add(item1Changed2);
+        testList.add(item2);
+        PriceList expected = new PriceList(testList);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void whatIsThePrice() {
+        int amount = 3;
+        //chosen item is item 2, its code is 19853
+
+        double expected = amount * item2.getPrice();
+
+        PriceList testList = new PriceList(new ArrayList<Item>());
+        testList.setItem(item);
+        testList.setItem(item1);
+        testList.setItem(item2);
+        double actual = testList.whatIsThePrice(item2.getCode(), amount);
+        boolean result = (expected == actual);
+
+        Assert.assertEquals(result, true);
     }
 }
